3f6de4534073735eda9231d3b9c06a78acb601fa
Kaye Xie <xiekaye@gmail.com>
2022-10-26 23:30:10 -0400

    seeding data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,6 +1,6 @@
 
 const sequelize = require ('../config/connection');
-const User = require('../models/User');
+const {User, Post, Comment} = require('../models');
 
 const userData = require ('./userData.json');
 
@@ -12,6 +12,21 @@ const seedDatabase = async() => {
         returning: true, 
     })
 
+    for (const post of postData) {
+        await Post.create({
+          ...post,
+          user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+      }
+    
+
+    for (const comment of commentData) {
+        await Comment.create({
+          ...comment,
+          user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+      }
+    
     process.exit(0);
 }
 
